c2c05651063b73fbbc8f3b80fea493eab4b4b30a
venkat <venkat.pv71@gmail.com>
2025-12-14 19:28:01 +0530

    final commit

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -4,7 +4,7 @@
 /// Get your API key from: https://www.themoviedb.org/settings/api
 class ApiConfig {
   // TODO: Replace this with your actual TMDB API key
-  static const String apiKey = 'YOUR_APY_KEY';
+  static const String apiKey = 'YOUR_APY_KEY ';
   static const String baseUrl = 'https://api.themoviedb.org/3';
   static const String imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
   static const String imageBaseUrlOriginal = 'https://image.tmdb.org/t/p/original';
