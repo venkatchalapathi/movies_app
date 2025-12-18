b6930773583c14bfc33aa2035e7232ebfd3af34d
venkat <venkat.pv71@gmail.com>
2025-12-18 22:18:29 +0530

    final commit

--- lib/data/models/movie_model.dart
+++ lib/data/models/movie_model.dart
@@ -24,6 +24,7 @@ class MovieModel extends Movie {
       id: json['id'] as int,
       title: json['title'] as String? ?? '',
       overview: json['overview'] as String? ?? '',
+
       posterPath: json['poster_path'] as String? ?? '',
       backdropPath: json['backdrop_path'] as String? ?? '',
       releaseDate: json['release_date'] as String? ?? '',
