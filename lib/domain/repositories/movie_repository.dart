1ee9b42d4e4b07adc758ff0898fc8b8f3bb221b3
venkat <venkat.pv71@gmail.com>
2025-12-20 22:56:24 +0530

    final commit

--- lib/domain/repositories/movie_repository.dart
+++ lib/domain/repositories/movie_repository.dart
@@ -1,6 +1,7 @@
 import 'package:movies_app/domain/entities/movie.dart';
 
 abstract class MovieRepository {
+
   Future<List<Movie>> getPopularMovies();
   Future<Movie> getMovieDetails(int movieId);
   Future<List<Movie>> getFavoriteMovies();
