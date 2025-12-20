ae64de8f6361e5a5636c028f5eeeae20a3119aad
venkat <venkat.pv71@gmail.com>
2025-12-20 22:55:40 +0530

    final commit

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -7,6 +7,7 @@ class ApiConfig {
   static const String apiKey = 'YOUR_APY_KEY ';
 
   static const String baseUrl = 'https://api.themoviedb.org/3';
+
   static const String imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
   static const String imageBaseUrlOriginal = 'https://image.tmdb.org/t/p/original';
 }
